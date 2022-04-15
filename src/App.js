fac995ff34d784a6de7e4acddf7d8aa11bb479c5
techshomez <danieltopeg@gmail.com>
2022-04-15 21:44:56 +0100

    hero component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Hero from './components/hero';
 import Navbar from './components/navbar/index';
+import Sidebar from './components/sidebar';
 import SubMenu from './components/submenu';
 
 function App() {
   return (
     <>
      <Navbar />
-     {/* Sidebar */}
+     <Sidebar />
      <Hero />
      <SubMenu />
     </>
